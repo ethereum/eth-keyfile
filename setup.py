e6eb586842c916047d417059292e0f5bc26e4dc5
Bhargavasomu <somubhargava97@gmail.com>
2019-01-14 22:30:01 +0530

    Remove cytoolz dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ deps = {
     'keyfile': [
         "eth-utils>=1.3.0,<2",
         "eth-keys>=0.2.1,<0.3.0",
-        "cytoolz>=0.9.0,<1",
         "pycryptodome>=3.6.6,<4",
     ],
     'test': [
