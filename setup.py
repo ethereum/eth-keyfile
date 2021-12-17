fc420b98e375571ebc5d7ce0ae38eeb515cea00d
kclowes <kclowes@users.noreply.github.com>
2021-12-17 10:37:48 -0700

    Add support for python 3.7

--- setup.py
+++ setup.py
@@ -71,5 +71,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
