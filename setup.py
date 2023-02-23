7cf2e2421c3926ead4dd063b1ddc695df3e8ce1e
kclowes <kclowes@users.noreply.github.com>
2023-02-23 10:21:25 -0700

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("./README.md") as readme:
 setup(
     name='eth-keyfile',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.6.0',
+    version='0.6.1',
     description=(
         "A library for handling the encrypted keyfiles used to store ethereum "
         "private keys."
